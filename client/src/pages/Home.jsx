f26f5472cbf7be514ff87d47b283e0a6e1003ac0
Mortymier <sanielmg@gmail.com>
2025-11-17 03:35:05 +0800

    Client: add Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,31 @@
+import { Link } from 'react-router-dom';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
+import hero from '../images/laptop_hero.jpg';
+import process from '../images/borrow_process.png';
+import '../styles/Home.css';
 
 export default function Home()
 {
     return (
         <>
             <Header/>
+            <main className="home-container">
+                <img src={hero} alt="A woman handing someone a laptop"/>
+                <div className="get-started-overlay">
+                    <h3> WE GOT YOU COVERED! </h3>
+                    <p>
+                        Need a laptop for your next task? <br/>
+                        We can help you get one from the CCS Office. <br/>
+                        Create an account now and start borrowing!
+                    </p>
+                    <Link> Start Borrowing </Link>
+                </div>
+                <div className="borrow-process">
+                    <h3> BORROWING PROCESS </h3>
+                    <img src={process} alt="Laptop borrow process"/>
+                </div>
+            </main>
             <Footer/>
         </>
     )
