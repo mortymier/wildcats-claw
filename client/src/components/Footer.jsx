2c33a495a7c708490fbd19321a46edd63487bbbe
Mortymier <sanielmg@gmail.com>
2025-11-17 03:07:58 +0800

    Client: add Footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import '../styles/Footer.css';
+
+export default function Footer()
+{
+    return (
+        <footer className="footer-container">
+            Wildcats CLAW - Campus Laptop Allocation Website
+        </footer>
+    );
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
 
 export default function Home()
 {
     return (
         <>
             <Header/>
+            <Footer/>
         </>
     )
 }
