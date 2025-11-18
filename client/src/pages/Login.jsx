e4b3992eb7de95e9ae6baf8b744cf1ca1f2d780b
Mortymier <sanielmg@gmail.com>
2025-11-19 02:59:39 +0800

    Client: connect Login page to backend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,6 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { loginUser } from '../api/auth.jsx';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
 import user_icon from '../images/user_icon.png';
@@ -7,12 +10,29 @@ import '../styles/Login.css';
 
 export default function Login()
 {
+    const navigate = useNavigate();
+    const [role, setRole] = useState('student');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleCancel = () => 
+    {
+        navigate('/');
+    };
+
+    const handleSubmit = async(e) =>
+    {
+        e.preventDefault();
+
+        const response = await loginUser(role, email, password);
+    }
+
     return (
         <>
             <title> Login - Wildcats CLAW </title>
             <Header/>
             <main className="login-container">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h1> Wildcats CLAW </h1>
                     <p> Login to your account </p>
                     <hr/>
@@ -23,7 +43,7 @@ export default function Login()
                                 <img src={user_icon} alt="Role icon"/>
                                 <label htmlFor="role"> Role </label>
                             </div>
-                            <select id="role">
+                            <select id="role" value={role} onChange={(e) => setRole(e.target.value)}>
                                 <option value="student"> STUDENT </option>
                                 <option value="staff"> STAFF </option>
                             </select>
@@ -34,7 +54,7 @@ export default function Login()
                                 <img src={email_icon} alt="Email icon"/>
                                 <label htmlFor="email"> Email </label>
                             </div>
-                            <input id="email" type="email" required/>
+                            <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                         </div>
                         {/* Password Input */}
                         <div className="login-input">
@@ -42,11 +62,11 @@ export default function Login()
                                 <img src={lock_icon} alt="Password icon"/>
                                 <label htmlFor="password"> Password </label>
                             </div>
-                            <input id="password" type="password" required/>
+                            <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
                         </div>
                         {/* Buttons */}
                         <div className="login-buttons">
-                            <button type="button"> Cancel </button>
+                            <button type="button" onClick={handleCancel}> Cancel </button>
                             <button type="submit"> Login </button>
                         </div>
                     </div>
