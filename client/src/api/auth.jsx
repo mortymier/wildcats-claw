b6faa5f2c3f5c4400e0795924fda1eb09af61ec1
Mortymier <sanielmg@gmail.com>
2025-11-19 02:57:48 +0800

    Client: add directory for API calls + authentication services

---
+++ client/src/api/auth.jsx
@@ -0,0 +1,76 @@
+// POST: Login user
+export const loginUser = async(role, email, password) =>
+{
+    try
+    {
+        const response = await fetch
+        (
+            `http://localhost:8080/api/${role}/login`,
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify
+                ({
+                    email: email,
+                    password: password
+                })
+            }
+        );
+
+        if(response.ok && role === 'student')
+        {
+            alert('Student login successful!');
+        }
+        else if(response.ok && role === 'student')
+        {
+            alert('Staff login successful!');
+        }
+        else if(response.status === 401)
+        {
+            throw new Error('Invalid role, email, or password');
+        }
+        else
+        {
+            throw new Error('Login failed');
+        }
+    }
+    catch(error)
+    {
+        alert(error.message);
+    }
+};
+
+// POST: Register student
+export const registerStudent = async(formData) =>
+{
+    try
+    {
+        const response = await fetch
+        (
+            'http://localhost:8080/api/student/register',
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData)
+            }
+        );
+
+        if(response.ok)
+        {
+            alert('Registration successful')
+            return response;
+        }
+        else if(response.status === 500 || response.status === 403)
+        {
+            throw new Error('Email is already used');
+        }
+        else
+        {
+            throw new Error('Registration failed');
+        }
+    }
+    catch(error)
+    {
+        alert(error.message);
+    }
+};
