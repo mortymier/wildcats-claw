6bfe91c8482f68b3a5997ad39a2d0e9876446b72
Mortymier <sanielmg@gmail.com>
2025-11-18 18:10:06 +0800

    Client: add Register page

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
+import '../styles/Register.css';
+
+export default function Register()
+{
+    return (
+        <>
+            <title> Register - Wildcats CLAW </title>
+            <Header/>
+            <main className="register-container">
+                <div>
+                    <h1> User Registration </h1>
+                    <p> Please select role: </p>
+                    <hr/>
+                    <Link> STUDENT </Link>
+                    <p> or </p>
+                    <Link> STAFF </Link>
+                </div>
+            </main>
+            <Footer/>
+        </>
+    );
+}
