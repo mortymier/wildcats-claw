00e4794ef2664bda88eeb8d67729c0a37c11244e
Mortymier <sanielmg@gmail.com>
2025-11-18 23:54:04 +0800

    Server: add Config layer + Spring Security configuration

---
+++ server/src/main/java/com/wildcatsclaw/server/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.wildcatsclaw.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig
+{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/student/**").permitAll()
+                .requestMatchers("/api/staff/**").permitAll()
+                .requestMatchers("/api/laptop/**").permitAll()
+                .requestMatchers("/api/schedule/**").permitAll()
+                .requestMatchers("/api/borrow/**").permitAll()
+                .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
