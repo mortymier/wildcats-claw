568a59862a391c45066e521d10d46c501cf4bbf7
Mortymier <sanielmg@gmail.com>
2025-11-18 23:04:46 +0800

    Client: add route for RegisterStaff page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home.jsx';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import RegisterStudent from './pages/RegisterStudent.jsx';
+import RegisterStaff from './pages/RegisterStaff.jsx';
 
 export default function App()
 {
@@ -14,6 +15,7 @@ export default function App()
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/register/student" element={<RegisterStudent/>}/>
+                <Route path="/register/staff" element={<RegisterStaff/>}/>
             </Routes>
         </BrowserRouter>
     );
