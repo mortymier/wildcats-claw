250910822ddf0394fd582fb1d2375a6674b9a71e
Mortymier <sanielmg@gmail.com>
2025-12-01 01:59:11 +0800

    Server: add functions for adding laptops and updating their status in LaptopService

--- server/src/main/java/com/wildcatsclaw/server/service/LaptopService.java
+++ server/src/main/java/com/wildcatsclaw/server/service/LaptopService.java
@@ -4,7 +4,11 @@ import com.wildcatsclaw.server.entity.LaptopEntity;
 import com.wildcatsclaw.server.repository.LaptopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import javax.naming.NameNotFoundException;
+import java.io.IOException;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class LaptopService
@@ -19,6 +23,21 @@ public class LaptopService
         return laptopRepository.save(laptop);
     }
 
+    public LaptopEntity addLaptop2(String brand, String model, String ram, String cpu, String storage, double price, MultipartFile image) throws IOException
+    {
+        LaptopEntity laptop = new LaptopEntity();
+
+        laptop.setBrand(brand);
+        laptop.setModel(model);
+        laptop.setRam(ram);
+        laptop.setCpu(cpu);
+        laptop.setStorage(storage);
+        laptop.setPrice(price);
+        laptop.setImage(image.getBytes());
+        
+        return laptopRepository.save(laptop);
+    }
+
     // GET ALL LAPTOPS
     public List<LaptopEntity> getAllLaptops()
     {
@@ -29,5 +48,27 @@ public class LaptopService
     public List<LaptopEntity> getAllLaptopsByStatus(LaptopEntity.LaptopStatus laptopstatus)
     {
         return laptopRepository.findByLaptopstatus(laptopstatus);
+    }
+
+    // UPDATE LAPTOP STATUS
+    public LaptopEntity updateLaptopStatus(LaptopEntity.LaptopStatus newstatus, LaptopEntity laptop)
+    {
+        LaptopEntity updatedLaptop = new LaptopEntity();
+
+        try
+        {
+            String brand = laptop.getBrand();
+            String model = laptop.getModel();
+            updatedLaptop = laptopRepository.findByBrandAndModel(brand, model).get();
+            updatedLaptop.setLaptopstatus(newstatus);
+        }
+        catch(NoSuchElementException e)
+        {
+            throw new NameNotFoundException("Laptop does not exist!");
+        }
+        finally
+        {
+            return laptopRepository.save(updatedLaptop);
+        }
     }
 }
