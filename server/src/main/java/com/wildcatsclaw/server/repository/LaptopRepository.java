16e9c9cbd23ee1327c1eb06c621cb8a359c1a2f2
Mortymier <sanielmg@gmail.com>
2025-12-01 01:57:54 +0800

    Server: add function to find laptop by brand and model in LaptopRepository

--- server/src/main/java/com/wildcatsclaw/server/repository/LaptopRepository.java
+++ server/src/main/java/com/wildcatsclaw/server/repository/LaptopRepository.java
@@ -4,10 +4,15 @@ import com.wildcatsclaw.server.entity.LaptopEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LaptopRepository extends JpaRepository<LaptopEntity, Long>
 {
     // Find all laptops based on laptop status
     List<LaptopEntity> findByLaptopstatus(LaptopEntity.LaptopStatus laptopstatus);
+
+    // Find laptop by brand and model
+    // Limitations: no two laptop must have the same brand and model
+    Optional<LaptopEntity> findByBrandAndModel(String brand, String model);
 }
