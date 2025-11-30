059b2a398dfd2cb11d78438d538b63edb8863135
Mortymier <sanielmg@gmail.com>
2025-12-01 02:20:49 +0800

    Client: add routes for student and staff pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,12 @@ import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import RegisterStudent from './pages/RegisterStudent.jsx';
 import RegisterStaff from './pages/RegisterStaff.jsx';
+import StudentDashboard from './pages/StudentDashboard.jsx';
+import StudentBrowse from './pages/StudentBrowse.jsx';
+import StudentSchedules from './pages/StudentSchedules.jsx';
+import StaffDashboard from './pages/StaffDashboard.jsx';
+import StaffBrowse from './pages/StaffBrowse.jsx';
+import StaffAddLaptop from './pages/StaffAddLaptop.jsx';
 
 export default function App()
 {
@@ -16,6 +22,12 @@ export default function App()
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/register/student" element={<RegisterStudent/>}/>
                 <Route path="/register/staff" element={<RegisterStaff/>}/>
+                <Route path="/student/dashboard" element={<StudentDashboard/>}/>
+                <Route path="/student/browse" element={<StudentBrowse/>}/>
+                <Route path="/student/schedules" element={<StudentSchedules/>}/>
+                <Route path="/staff/dashboard" element={<StaffDashboard/>}/>
+                <Route path="/staff/browse" element={<StaffBrowse/>}/>
+                <Route path="/staff/addlaptop" element={<StaffAddLaptop/>}/>
             </Routes>
         </BrowserRouter>
     );
